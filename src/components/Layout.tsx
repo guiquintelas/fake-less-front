482d22bc05b5ee63656334b4486c00e353d2a34e
Guilherme Frota <guiquintelas96@gmail.com>
2020-08-31 13:47:58 -0300

    add logged user menu in header

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import {
-  AppBar, Toolbar, IconButton, makeStyles, Typography,
+  AppBar, Toolbar, IconButton, makeStyles, Typography, Box, Button, MenuItem,
 } from '@material-ui/core';
-import { Menu } from 'mdi-material-ui';
+import { Menu as MenuIcon } from 'mdi-material-ui';
+import { useHistory } from 'react-router-dom';
+import Menu from './Menu';
+import { useUser } from '../contexts/UserContext';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -18,18 +21,44 @@ const useStyles = makeStyles((theme) => ({
 
 const Layout: React.FC = () => {
   const classes = useStyles();
+  const history = useHistory();
+  const { user, setUser } = useUser();
 
   return (
     <>
       <AppBar position="relative" style={{ height: 'fit-content' }}>
         <Toolbar>
           <IconButton edge="start" className={classes.menuButton} color="inherit" ria-label="menu">
-            <Menu />
+            <MenuIcon />
           </IconButton>
 
           <Typography variant="h6" className={classes.title}>
             Fake Less
           </Typography>
+
+          <Box>
+            {user ? (
+              <Menu
+                anchor={(
+                  <Button color="inherit" style={{ textTransform: 'none' }}>
+                    {user?.username}
+                  </Button>
+              )}
+              >
+                <MenuItem onClick={() => {
+                  setUser(undefined);
+                  history.push('/login');
+                }}
+                >
+                  Logout
+                </MenuItem>
+              </Menu>
+            ) : (
+              <Button color="inherit" onClick={() => history.push('/login')}>
+                Logar
+              </Button>
+            )}
+          </Box>
         </Toolbar>
       </AppBar>
 
