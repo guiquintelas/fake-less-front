e3cf8a80d15ea57d292827ab9d5979c7fdec3098
Guilherme Frota <guiquintelas96@gmail.com>
2020-09-01 15:13:25 -0300

    fix textfield warnings

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -24,7 +24,7 @@ const TextField: React.FC<TextFieldProps> = (
   // it reflects in formik state
   useEffect(() => {
     setFieldValue(name, value ?? '');
-  }, [value]);
+  }, [name, value, setFieldValue]);
 
   const iconAdornment = icon ? (
     <InputAdornment position="start">
