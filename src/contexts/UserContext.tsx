3d8d92dd78b87e5fb0559a2d2428f0cf6d6e8af7
Guilherme Frota <guiquintelas96@gmail.com>
2020-09-01 11:10:19 -0300

    persist user in localStorage

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,25 +1,49 @@
 import React, {
-  createContext, useState, useContext,
+  createContext, useState, useContext, useEffect,
 } from 'react';
 
-type User = {
+type User = undefined | {
     username: string,
     password: string,
-}
+};
 
 type UserContextType = {
-  user?: User,
-  setUser: React.Dispatch<React.SetStateAction<User | undefined>>,
+  user: User,
+  setUser: React.Dispatch<React.SetStateAction<User>>,
+}
+
+const USER_STORAGE = 'user';
+
+const loadedJsonUserData = localStorage.getItem(USER_STORAGE);
+let defaultUser: User;
+
+try {
+  defaultUser = loadedJsonUserData
+    ? JSON.parse(loadedJsonUserData)
+    : undefined;
+} catch (_) {
+  // if json is corrupted
+  defaultUser = undefined;
 }
 
 export const UserContext = createContext<UserContextType>({
+  user: defaultUser,
   setUser: () => {
-    throw new Error('state do provider ainda nao iniciado!');
+    throw new Error('state not initialized');
   },
 });
 
 const UserProvider: React.FC = ({ children }) => {
-  const [user, setUser] = useState<User>();
+  const [user, setUser] = useState<User>(defaultUser);
+
+  useEffect(() => {
+    if (user) {
+      localStorage.setItem(USER_STORAGE, JSON.stringify(user));
+    } else {
+      localStorage.removeItem(USER_STORAGE);
+    }
+  }, [user]);
+
   return (
     <UserContext.Provider value={{ user, setUser }}>
       {children}
