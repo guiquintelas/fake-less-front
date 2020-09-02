82af78d5a72248f87c6060ca30322bac836e8dee
Guilherme Frota <guiquintelas96@gmail.com>
2020-09-02 19:00:15 -0300

    add like button & 'coment' textfield

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -2,58 +2,64 @@ import React from 'react';
 import {
   Paper, Box, Typography, Divider, Button, TextField,
 } from '@material-ui/core';
-import { HeartOutline } from 'mdi-material-ui';
-import { Post as PostType } from '../contexts/FeedContext';
+import { HeartOutline, Heart } from 'mdi-material-ui';
+import { Post as PostType, useFeedContext } from '../contexts/FeedContext';
 
 export interface PostProps {
   post: PostType
 }
 
-const Post: React.SFC<PostProps> = ({ post }) => (
-  <Paper>
-    <Box display="flex" flexDirection="column" p={2}>
-      <Box>
-        <Typography variant="body1">
-          {post.createdBy}
-        </Typography>
+const Post: React.SFC<PostProps> = ({ post }) => {
+  const { toggleLikePost } = useFeedContext();
 
-        <Typography variant="caption">
-          {post.createdAt.toLocaleString()}
-        </Typography>
-      </Box>
+  return (
+    <Paper>
+      <Box display="flex" flexDirection="column" p={2}>
+        <Box>
+          <Typography variant="body1">
+            {post.createdBy}
+          </Typography>
 
-      <Box pt={2}>
-        <Typography variant="body2">
-          {post.content}
-        </Typography>
-      </Box>
-
-      <Box my={2}>
-        <Divider />
-      </Box>
-
-      <Box display="flex" alignItems="center">
-        <Box pr={2} flexGrow={0}>
-          <Button
-            startIcon={<HeartOutline />}
-          >
-            Curtir
-          </Button>
+          <Typography variant="caption">
+            {post.createdAt.toLocaleString()}
+          </Typography>
         </Box>
 
-        <Box flexGrow={2}>
-          <TextField
-            size="small"
-            placeholder="Comentar ..."
-            style={{ width: '100%' }}
-            InputProps={{
-              style: { marginBottom: 0 },
-            }}
-          />
+        <Box pt={2}>
+          <Typography variant="body2">
+            {post.content}
+          </Typography>
+        </Box>
+
+        <Box my={2}>
+          <Divider />
+        </Box>
+
+        <Box display="flex" alignItems="center">
+          <Box pr={2} flexGrow={0}>
+            <Button
+              startIcon={post.liked ? <Heart /> : <HeartOutline />}
+              onClick={() => toggleLikePost(post.id)}
+              color={post.liked ? 'secondary' : 'default'}
+            >
+              {post.liked ? 'descurtir' : 'curtir'}
+            </Button>
+          </Box>
+
+          <Box flexGrow={2}>
+            <TextField
+              size="small"
+              placeholder="Comentar ..."
+              style={{ width: '100%' }}
+              InputProps={{
+                style: { marginBottom: 0 },
+              }}
+            />
+          </Box>
         </Box>
       </Box>
-    </Box>
-  </Paper>
-);
+    </Paper>
+  );
+};
 
 export default Post;
