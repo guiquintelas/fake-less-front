751616b4699047fc30136ee0c52c071d407ede9d
Guilherme Frota <guiquintelas96@gmail.com>
2020-09-02 13:37:04 -0300

    add id to posts
    fixes duplicate keys

--- src/contexts/FeedContext.tsx
+++ src/contexts/FeedContext.tsx
@@ -1,8 +1,10 @@
 import React, {
   createContext, useState, useContext,
 } from 'react';
+import { v4 as uuid } from 'uuid';
 
 type Post = {
+  id: string,
   content: string,
   createdAt: Date,
   createdBy: string,
@@ -19,9 +21,18 @@ type FeedContextType = {
 
 const defaultFeed: Feed = {
   posts: [
-    { content: 'post 1', createdBy: 'Guilherme Frota', createdAt: new Date() },
-    { content: 'post 2', createdBy: 'Guilherme Frota', createdAt: new Date() },
-    { content: 'post 3', createdBy: 'Guilherme Frota', createdAt: new Date() },
+    {
+      id: uuid(), content: 'post 1', createdBy: 'Guilherme Frota', createdAt: new Date(),
+    },
+    {
+      id: uuid(), content: 'post 2', createdBy: 'Guilherme Frota', createdAt: new Date(),
+    },
+    {
+      id: uuid(), content: 'post 3', createdBy: 'Guilherme Frota', createdAt: new Date(),
+    },
+    {
+      id: uuid(), content: 'post 4', createdBy: 'Guilherme Frota', createdAt: new Date(),
+    },
   ],
 };
 
