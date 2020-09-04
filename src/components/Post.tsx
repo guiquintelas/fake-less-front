7dc171ae01fe891029fde19a1fc117f8e6f4d615
Guilherme Frota <guiquintelas96@gmail.com>
2020-09-04 12:36:28 -0300

    display comments in Post component

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Paper, Box, Typography, Divider, Button, TextField } from '@material-ui/core';
+import { Paper, Box, Typography, Divider, Button, TextField, Grid } from '@material-ui/core';
 import { HeartOutline, Heart } from 'mdi-material-ui';
 import { Post as PostType, useFeedContext } from '../contexts/FeedContext';
 
@@ -12,43 +12,64 @@ const Post: React.SFC<PostProps> = ({ post }) => {
 
   return (
     <Paper>
-      <Box display="flex" flexDirection="column" p={2}>
-        <Box>
-          <Typography variant="body1">{post.createdBy}</Typography>
+      <Box p={2}>
+        <Grid container direction="column" spacing={2}>
+          <Grid item>
+            <Typography variant="body1">{post.createdBy}</Typography>
 
-          <Typography variant="caption">{post.createdAt.toLocaleString()}</Typography>
-        </Box>
+            <Typography variant="caption">{post.createdAt.toLocaleString()}</Typography>
+          </Grid>
 
-        <Box pt={2}>
-          <Typography variant="body2">{post.content}</Typography>
-        </Box>
+          <Grid item>
+            <Typography variant="body2">{post.content}</Typography>
+          </Grid>
 
-        <Box my={2}>
-          <Divider />
-        </Box>
+          <Grid item>
+            <Divider />
+          </Grid>
 
-        <Box display="flex" alignItems="center">
-          <Box pr={2} flexGrow={0}>
-            <Button
-              startIcon={post.liked ? <Heart /> : <HeartOutline />}
-              onClick={() => toggleLikePost(post.id)}
-              color={post.liked ? 'secondary' : 'default'}
-            >
-              {post.liked ? 'descurtir' : 'curtir'}
-            </Button>
-          </Box>
+          <Grid container item wrap="nowrap">
+            <Box pr={2} flexGrow={0}>
+              <Button
+                startIcon={post.liked ? <Heart /> : <HeartOutline />}
+                onClick={() => toggleLikePost(post.id)}
+                color={post.liked ? 'secondary' : 'default'}
+              >
+                {post.liked ? 'descurtir' : 'curtir'}
+              </Button>
+            </Box>
 
-          <Box flexGrow={2}>
-            <TextField
-              size="small"
-              placeholder="Comentar ..."
-              style={{ width: '100%' }}
-              InputProps={{
-                style: { marginBottom: 0 },
-              }}
-            />
-          </Box>
-        </Box>
+            <Box flexGrow={2}>
+              <TextField
+                size="small"
+                placeholder="Comentar ..."
+                style={{ width: '100%' }}
+                InputProps={{
+                  style: { marginBottom: 0 },
+                }}
+              />
+            </Box>
+          </Grid>
+
+          {post.comments.length > 0 && (
+            <Grid item spacing={1} direction="column">
+              <Box>
+                <Typography variant="caption">Comentários</Typography>
+              </Box>
+
+              {post.comments.map((comment) => (
+                <Box key={comment.id} display="flex" alignItems="center">
+                  <Box pr={1} flexGrow={0}>
+                    <Typography variant="subtitle2">{comment.createdBy}</Typography>
+                  </Box>
+                  <Box flexGrow={1}>
+                    <Typography variant="body2">{comment.content}</Typography>
+                  </Box>
+                </Box>
+              ))}
+            </Grid>
+          )}
+        </Grid>
       </Box>
     </Paper>
   );
