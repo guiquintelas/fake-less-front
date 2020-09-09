ed14dd21280220aafe47806470c297d8acb6f760
Guilherme Frota <guiquintelas96@gmail.com>
2020-09-09 17:51:27 -0300

    fix InputProps not forwarding & add ref support

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -4,46 +4,38 @@ import { useField, useFormikContext } from 'formik';
 import { InputAdornment } from '@material-ui/core';
 
 export type TextFieldProps = MuiTextFieldProps & {
-  name: string,
-  icon?: JSX.Element,
+  name: string;
+  icon?: JSX.Element;
 };
 
-const TextField: React.FC<TextFieldProps> = (
-  {
-    name,
-    icon,
-    disabled,
-    value,
-    ...props
+const TextField = React.forwardRef<HTMLInputElement, TextFieldProps>(
+  ({ name, icon, disabled, value, InputProps, ...props }, ref) => {
+    const [field, meta] = useField(name);
+    const { isSubmitting, setFieldValue } = useFormikContext();
+
+    // every time the prop 'value' changes
+    // it reflects in formik state
+    useEffect(() => {
+      setFieldValue(name, value ?? '');
+    }, [name, value, setFieldValue]);
+
+    const iconAdornment = icon ? <InputAdornment position="start">{icon}</InputAdornment> : null;
+
+    return (
+      <MuiTextfield
+        inputRef={ref}
+        {...field}
+        {...props}
+        error={meta.touched && !!meta.error}
+        helperText={meta.touched && meta.error ? meta.error : null}
+        disabled={disabled || isSubmitting}
+        InputProps={{
+          ...InputProps,
+          startAdornment: iconAdornment,
+        }}
+      />
+    );
   },
-) => {
-  const [field, meta] = useField(name);
-  const { isSubmitting, setFieldValue } = useFormikContext();
-
-  // every time the prop 'value' changes
-  // it reflects in formik state
-  useEffect(() => {
-    setFieldValue(name, value ?? '');
-  }, [name, value, setFieldValue]);
-
-  const iconAdornment = icon ? (
-    <InputAdornment position="start">
-      {icon}
-    </InputAdornment>
-  ) : null;
-
-  return (
-    <MuiTextfield
-      {...field}
-      {...props}
-      error={meta.touched && !!meta.error}
-      helperText={meta.touched && meta.error ? meta.error : null}
-      disabled={disabled || isSubmitting}
-      InputProps={{
-        startAdornment: iconAdornment,
-      }}
-    />
-  );
-};
+);
 
 export default TextField;
