6b49e00ef2c606a809ce07416f5bbe13bc0fc889
Guilherme Frota <guiquintelas96@gmail.com>
2020-09-17 10:49:48 -0300

    remove unecessary request interceptors
    dotnet already accepts camelCase variables

--- src/api.ts
+++ src/api.ts
@@ -6,24 +6,6 @@ const api = axios.create({
   withCredentials: true,
 });
 
-api.interceptors.request.use(
-  (config) => {
-    const camelCaseData: { [key: string]: any } = {};
-
-    // converts to C# case
-    // Ex password => Password
-    // Ex fullNmae => FullName
-    Object.keys(config.data).forEach((key) => {
-      const firstLetter = key[0];
-      const newKey = firstLetter.toUpperCase() + key.substr(1);
-      camelCaseData[newKey] = config.data[key];
-    });
-
-    return config;
-  },
-  (_) => _,
-);
-
 api.interceptors.response.use(
   (_) => _,
   (error) => {
