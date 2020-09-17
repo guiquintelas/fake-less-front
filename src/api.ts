9f5ed87c241f81b6c78842591edbbae193323a0c
Guilherme Frota <guiquintelas96@gmail.com>
2020-09-16 21:47:05 -0300

    add axios interceptor to parse request data case

--- src/api.ts
+++ src/api.ts
@@ -6,5 +6,22 @@ const api = axios.create({
   withCredentials: true,
 });
 
+api.interceptors.request.use(
+  (config) => {
+    const camelCaseData: { [key: string]: any } = {};
+
+    // converts to C# case
+    // Ex password => Password
+    // Ex fullNmae => FullName
+    Object.keys(config.data).forEach((key) => {
+      const firstLetter = key[0];
+      const newKey = firstLetter.toUpperCase() + key.substr(1);
+      camelCaseData[newKey] = config.data[key];
+    });
+
+    return config;
+  },
+  (_) => _,
+);
 
 export default api;
