b6d97eb37db94959d313a73f64359d9899607512
Guilherme Frota <guiquintelas96@gmail.com>
2020-09-16 21:46:32 -0300

    add axios specific api provider

---
+++ src/api.ts
@@ -0,0 +1,10 @@
+/* eslint-disable no-console */
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:5000/api',
+  withCredentials: true,
+});
+
+
+export default api;
