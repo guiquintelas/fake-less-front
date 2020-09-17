9cd0f6d4a7edfe8cda0629a48077f22afbc6ab98
Guilherme Frota <guiquintelas96@gmail.com>
2020-09-16 21:47:46 -0300

    add axios interceptor in response to handle many error responses

--- src/api.ts
+++ src/api.ts
@@ -24,4 +24,32 @@ api.interceptors.request.use(
   (_) => _,
 );
 
+api.interceptors.response.use(
+  (_) => _,
+  (error) => {
+    // api returns only a string as response
+    // should handle as the error msg
+    if (typeof error.response.data === 'string') {
+      console.error(error);
+      console.error(error.response);
+      return Promise.reject(Error(error.response.data));
+    }
+
+    // api returns an array os errors
+    // should handle as validation error
+    // therefore something was sent wrong
+    if (error.response.data.errors) {
+      console.error('Validation Error');
+      console.error(error.response.data.errors);
+    } else {
+      // any ohter situation log
+      // entire error and response
+      console.error(error);
+      console.error(error.response);
+    }
+
+    return Promise.reject(Error('Inernal Error'));
+  },
+);
+
 export default api;
