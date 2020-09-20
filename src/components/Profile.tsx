16131c2b9849dd7d8c16d09b614bdf43f342aaa1
Guilherme Frota <guiquintelas96@gmail.com>
2020-09-20 19:04:28 -0300

    make Profile fetch data via api

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,100 +1,123 @@
-import { Grid, Box, Typography, Avatar, Button } from '@material-ui/core';
-import React from 'react';
+import { Grid, Box, Typography, Avatar, Button, CircularProgress } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import { Email, MapMarker, CakeVariant, ArrowLeft } from 'mdi-material-ui';
 import { grey } from '@material-ui/core/colors';
 import Feed from './Feed';
 import FeedProvider from '../contexts/FeedContext';
-import { useUserContext } from '../contexts/UserContext';
+import { UserAPI, useUserContext, UserResponse } from '../contexts/UserContext';
+import api from '../api';
+import { useSnackBarContext } from '../contexts/SnackBarContext';
 
 const Profile: React.FC = () => {
-  const { profileId } = useParams<{ profileId: string }>();
+  const [user, setUser] = useState<null | UserAPI>(null);
+  const { userId } = useParams<{ userId: string }>();
   const history = useHistory();
+  const { snackBar } = useSnackBarContext();
   const { user: loggedUser } = useUserContext();
 
-  const user = {
-    id: 1,
-    profileId: 1,
-    name: 'Teste',
-    lastName: 'Sobrenome',
-    email: 'email@email.com',
-    location: 'Rio de Janeiro',
-    birthDate: new Date(),
-    avatarUrl: 'https://avatars2.githubusercontent.com/u/29166076?s=460&u=38c72ddb1aaa23b9350119d7db2645e9a2c3e4d1&v=4',
-  };
+  useEffect(() => {
+    const fetchUser = async () => {
+      let result: UserResponse;
+
+      try {
+        result = await api.get(`/usuario/${userId}`);
+      } catch (error) {
+        snackBar('User not found!', 'danger');
+        history.push('/');
+        return;
+      }
+
+      setUser(result.data);
+    };
+
+    fetchUser();
+  }, [userId]);
 
   return (
     <>
       <Grid item md={4}>
         <Box pr={2}>
           <Grid container direction="column" spacing={1}>
-            <Grid item>
-              <Box display="flex" p={4}>
-                <Avatar style={{ width: '90%', height: 'auto', margin: 'auto' }} alt={user.name} src={user.avatarUrl} />
-              </Box>
-
-              <Box width="fit-content" m="auto" pb={4}>
-                <Typography variant="h4">{`${user.name} ${user.lastName}`}</Typography>
-
-                <Box color={grey[600]} pt={2} m="auto" width="fit-content">
-                  <Box display="flex" pb={1}>
-                    <Box mr={1}>
-                      <Email />
-                    </Box>
-                    <Typography>{user.email}</Typography>
+            {user ? (
+              <Grid item>
+                <Box display="flex" p={4}>
+                  <Box position="relative" width="90%" margin="auto" pb="90%">
+                    <Avatar
+                      style={{ width: '100%', height: '100%', position: 'absolute', top: 0, left: 0 }}
+                      alt={user.nome}
+                      src=""
+                    />
                   </Box>
+                </Box>
 
-                  {user.location && (
-                    <Box display="flex" alignItems="center" pb={1}>
+                <Box width="fit-content" m="auto" pb={4}>
+                  <Typography variant="h4">{`${user.nome} ${user.sobrenome}`}</Typography>
+
+                  <Box color={grey[600]} pt={2} m="auto" width="fit-content">
+                    <Box display="flex" pb={1}>
                       <Box mr={1}>
-                        <MapMarker />
+                        <Email />
                       </Box>
-                      <Typography>{user.location}</Typography>
-                    </Box>
-                  )}
-
-                  {user.birthDate && (
-                    <Box display="flex" alignItems="center">
-                      <Box mr={1}>
-                        <CakeVariant />
-                      </Box>
-                      <Typography>{user.birthDate.toDateString()}</Typography>
-                    </Box>
-                  )}
-
-                  <Box pt={4}>
-                    <Box pb={2}>
-                      <Button
-                        style={{ textTransform: 'none', width: '100%' }}
-                        onClick={() => {
-                          history.push('/');
-                        }}
-                        color="primary"
-                        variant="contained"
-                        disableElevation
-                        disabled={loggedUser?.profileId === user.profileId}
-                      >
-                        Follow
-                      </Button>
+                      <Typography>{user.email}</Typography>
                     </Box>
 
-                    <Box>
-                      <Button
-                        style={{ textTransform: 'none', width: '100%', justifyContent: 'flex-start' }}
-                        onClick={() => {
-                          history.push('/');
-                        }}
-                      >
-                        <Box display="flex" pr={2}>
-                          <ArrowLeft />
+                    {user.localidade && (
+                      <Box display="flex" alignItems="center" pb={1}>
+                        <Box mr={1}>
+                          <MapMarker />
                         </Box>
-                        Back to Feed
-                      </Button>
+                        <Typography>{user.localidade}</Typography>
+                      </Box>
+                    )}
+
+                    {user.aniversario && (
+                      <Box display="flex" alignItems="center">
+                        <Box mr={1}>
+                          <CakeVariant />
+                        </Box>
+                        <Typography>{new Date(user.aniversario).toLocaleString().split(' ')[0]}</Typography>
+                      </Box>
+                    )}
+
+                    <Box pt={4}>
+                      <Box pb={2}>
+                        <Button
+                          style={{ textTransform: 'none', width: '100%' }}
+                          onClick={() => {
+                            history.push('/');
+                          }}
+                          color="primary"
+                          variant="contained"
+                          disableElevation
+                          disabled={loggedUser?.id === user.usuarioId}
+                        >
+                          Follow
+                        </Button>
+                      </Box>
+
+                      <Box>
+                        <Button
+                          style={{ textTransform: 'none', width: '100%', justifyContent: 'flex-start' }}
+                          onClick={() => {
+                            history.push('/');
+                          }}
+                        >
+                          <Box display="flex" pr={2}>
+                            <ArrowLeft />
+                          </Box>
+                          Back to Feed
+                        </Button>
+                      </Box>
                     </Box>
                   </Box>
                 </Box>
+              </Grid>
+            ) : (
+              <Box display="flex" height="50vh" alignItems="center" justifyContent="center">
+                <CircularProgress />
               </Box>
-            </Grid>
+            )}
           </Grid>
         </Box>
       </Grid>
@@ -102,7 +125,7 @@ const Profile: React.FC = () => {
       <Grid item container alignItems="center" xs={12} md={8}>
         <Grid item style={{ width: '100%' }}>
           <FeedProvider>
-            <Feed userId={profileId} />
+            <Feed userId={userId} />
           </FeedProvider>
         </Grid>
       </Grid>
