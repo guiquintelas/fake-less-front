7e194ee594601ee4f25831cb80dc56859cfc5c24
Guilherme Frota <guiquintelas96@gmail.com>
2020-09-21 16:20:29 -0300

    Updates

---
+++ precache-manifest.bbf9f0350b8dcfe11a236fc30a44a674.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ee31e48c8d97f4a434ff71e82b50f38",
+    "url": "/fake-less-front/index.html"
+  },
+  {
+    "revision": "91e9723fc60ff464d649",
+    "url": "/fake-less-front/static/js/2.c1e02b28.chunk.js"
+  },
+  {
+    "revision": "c80b09f21c3dd2137be347c65d78f97d",
+    "url": "/fake-less-front/static/js/2.c1e02b28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3376c31238e14270d0fd",
+    "url": "/fake-less-front/static/js/main.78d95e0b.chunk.js"
+  },
+  {
+    "revision": "f76bf20245b6b43965ea",
+    "url": "/fake-less-front/static/js/runtime-main.6ded1a00.js"
+  }
+]);
