856b1f2254f77602aa50bd76b6f33c64f31d693a
Guilherme Frota <guiquintelas96@gmail.com>
2020-09-21 15:56:36 -0300

    add post img UI

--- src/components/NewPost.tsx
+++ src/components/NewPost.tsx
@@ -1,33 +1,67 @@
 import { Box, Button, InputBase, Paper } from '@material-ui/core';
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { useFeedContext } from '../contexts/FeedContext';
 import { useUserContext } from '../contexts/UserContext';
 
 const NewPost: React.FC = () => {
   const { user } = useUserContext();
-  const [post, setPost] = useState('');
+  const [content, setContent] = useState('');
+  const [img, setImg] = useState<File | null>(null);
   const { addPost } = useFeedContext();
+  const uploadInputRef = useRef<HTMLInputElement>(null);
 
   return (
     <Paper>
+      {img && (
+        <Box display="flex" justifyContent="center">
+          <img src={URL.createObjectURL(img)} alt="post img" style={{ width: '100%' }} />
+        </Box>
+      )}
+
       <Box p={2} mb={2}>
         <InputBase
           multiline
-          value={post}
-          onChange={(e) => setPost(e.target.value)}
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
           placeholder={`Hey ${user!.name ?? user!.email}! Share something with us!`}
           style={{
             width: '100%',
           }}
         />
         <Box display="flex" justifyContent="flex-end">
+          <Box mr="auto">
+            <Button
+              disableElevation
+              onClick={() => {
+                uploadInputRef.current!.click();
+              }}
+            >
+              Add a picture
+              <input
+                ref={uploadInputRef}
+                style={{ display: 'none' }}
+                type="file"
+                accept="image/x-png,image/gif,image/jpeg"
+                onChange={(e) => {
+                  const file = e.target.files ? e.target.files[0] : null;
+
+                  if (!file) {
+                    return;
+                  }
+
+                  setImg(file);
+                }}
+              />
+            </Button>
+          </Box>
+
           <Button
             color="primary"
-            disabled={!post}
+            disabled={!content}
             onClick={() => {
-              setPost('');
+              setContent('');
               addPost({
-                content: post,
+                content,
                 createdAt: new Date(),
                 createdBy: {
                   id: 1,
